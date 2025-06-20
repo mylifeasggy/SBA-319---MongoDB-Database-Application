d3e656efe32f71818ee211a054cc062889e0b44b
mylifeasggy <ginellysmsuarez@gmail.com>
2025-06-20 18:36:21 -0400

    menu update

--- routes/menu.js
+++ routes/menu.js
@@ -5,52 +5,56 @@ const menuRoute = express.Router();
 
 
 
-menuRoute.get('/',async (req,res)=>{
- const menus = await Menu.find({})
+menuRoute.get('/', async (req, res) => {
+    const menus = await Menu.find({})
 
- res.json(menus);
+    res.json(menus);
 
 });
 
 
 try {
 
- menuRoute.post('/', (req,res)=>{
+    menuRoute.post('/', (req, res) => {
 
-    const { plate_name, plate_description,serving_size, price,vegan } = req.body;
+        const { plate_name, plate_description, serving_size, price, vegan } = req.body;
 
-    const newPlate = {
-        plate_name, 
-        plate_description,
-        serving_size, 
-        price,
-        vegan
-    }
-    const result =  newPlate.save()
-    
-    return res.status(201).json({ message:'New plate added successfully', result });
+        const newPlate = {
+            plate_name,
+            plate_description,
+            serving_size,
+            price,
+            vegan
+        }
+        const result = newPlate.save()
 
- });
+        return res.status(201).json({ message: 'New plate added successfully', result });
 
-}catch{
+    });
+
+} catch {
     console.log(e.message);
     res.status(400).json({ message: e.message })
 
 }
 
-menuRoute.put('/:id', async(req, res)=> {
-    const {id} = req.params;
-    const {price} = req.params.price
-    let plate = await Menu.findbyIdAndUpdate(id, {price});
-    plate.save()
-    res.status(200).json({ message: 'Plate updated', plate });
-})
 
+menuRoute.route('/:id')
 
-menuRoute.delete('/:id',async (req,res)=>{
-    const {id} = req.params;
-    let plate = await Menu.findbyIdAndDelete(id)
-    return res.status(204).json({message: 'Plate deleted', plate});
-});
+    .put('/:id', async (req, res) => {
+
+        const { id } = req.params;
+        const { price } = req.params.price
+        let plate = await Menu.findbyIdAndUpdate(id, { price });
+        plate.save()
+        res.status(200).json({ message: 'Plate updated', plate });
+
+    }).delete('/:id', async (req, res) => {
+
+        const { id } = req.params;
+        let plate = await Menu.findbyIdAndDelete(id)
+        return res.status(204).json({ message: 'Plate deleted', plate });
+        
+    });
 
 export default menuRoute;
