3a338d94db068a123e97f288288dd217bfaad993
mylifeasggy <ginellysmsuarez@gmail.com>
2025-06-20 14:36:59 -0400

    review route

---
+++ routes/reviews.js
@@ -0,0 +1,15 @@
+import express from "express";
+import Reviews from "../models/reviewsSchema.js"
+
+const reviewsRouter = express.Router()
+
+
+try{
+
+
+}catch {
+
+    
+}
+
+export default reviewsRouter
