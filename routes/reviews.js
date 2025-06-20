4856927435779936c95a36d6a911bc2d214d1f4a
mylifeasggy <ginellysmsuarez@gmail.com>
2025-06-20 15:07:14 -0400

    reviews routes

--- routes/reviews.js
+++ routes/reviews.js
@@ -4,12 +4,49 @@ import Reviews from "../models/reviewsSchema.js"
 const reviewsRouter = express.Router()
 
 
+reviewsRouter.get('/', async (req, res)=>{
+    let reviews = await Reviews.find({})
+    res.json(reviews)
+    console.log(reviews)
+})
+
+
 try{
+    reviewsRouter.post('/', (req, res)=> {
+        const {email, rating, comment} = req.body
 
+        const newReview = new Reviews ({
+            email,
+            rating,
+            comment 
+        });
 
-}catch {
+        const result = newReview.save()
 
-    
+        res.status(200).json({message: "Review created", result});
+    });
+
+}catch (e){
+    res.status(400).json({message:e.message})
+
+}
+
+try {
+    reviewsRouter.delete('/:id', async (req,res)=> {
+        const review = await Reviews.findOneAndDelete(req.params.id)
+
+        if(!review) {
+            res.status(204).json({message: 'Content no found'})
+        }
+
+        res.status(200).json({message:'Review delete '})
+        return review
+
+    });
+
+} catch (e) {
+    console.log(e.message)
+    throw e
 }
 
 export default reviewsRouter
