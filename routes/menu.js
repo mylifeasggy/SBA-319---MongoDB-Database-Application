e28ad5dc34c87e10e10d2a41de260ceec2379fbd
mylifeasggy <ginellysmsuarez@gmail.com>
2025-06-20 18:12:26 -0400

    menu route

--- routes/menu.js
+++ routes/menu.js
@@ -3,11 +3,39 @@ import Menu from "../models/menuSchema.js"
 
 const menuRoute = express.Router();
 
+
+
+menuRoute.get('/',async (req,res)=>{
+ const menus = await Menu.find({})
+
+ res.json(menus);
+
+});
+
+
 try {
 
-}catch{
+ menuRoute.post('/', (req,res)=>{
 
+    const { plate_name, plate_description,serving_size, price,vegan } = req.body;
+
+    const newPlate = {
+        plate_name, 
+        plate_description,
+        serving_size, 
+        price,
+        vegan
+    }
+    const result =  newPlate.save()
     
+    return res.status(201).json({ message:'New plate added successfully', result });
+
+ });
+
+}catch{
+    console.log(e.message);
+    res.status(400).json({ message: e.message })
+
 }
 
 
