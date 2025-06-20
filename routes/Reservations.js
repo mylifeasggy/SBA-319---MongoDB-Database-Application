3d887b2e2a87855f78c03ec9fab41daa15eebcde
mylifeasggy <ginellysmsuarez@gmail.com>
2025-06-20 15:28:19 -0400

    fixing reservations routes

--- routes/Reservations.js
+++ routes/Reservations.js
@@ -7,13 +7,13 @@ const router = express.Router();
 router.use(express.json());
 
 router.get('/', async (req, res) => {
-    try{
-let reservations = await Reservations.find({});
-    console.log(reservations)
-    res.send(reservations);
+    try {
+        let reservations = await Reservations.find({});
+        console.log(reservations)
+        res.send(reservations);
 
-    }catch (e) {
-        res.status(500).json({message: e.message});
+    } catch (e) {
+        res.status(500).json({ message: e.message });
     }
 });
 
@@ -35,35 +35,39 @@ try {
         const result = newReservation.save()
         console.log(newReservation)
 
-        return res.status(201).send({ message: 'Reservartion created successfully', result })
+        return res.status(201).json({ message: 'Reservartion created successfully', result })
     });
 
 } catch (e) {
     console.log(e.message);
-    res.status(400).json({message:e.message})
+    res.status(400).json({ message: e.message })
 }
 
 router.route("/:id")
 
-.get(async (req,res)=> {
+    .get(async (req, res) => {
+        const {id}= req.params
 
-    let reservation = await Reservations.findById({id});
-    if (reservation) {
-        return {reservation}
-    }else {
-         return  res.status(404).send('Reservation not found')
-    }
+        let reservation = await Reservations.findById({ id });
+        if (reservation) {
+            return { reservation }
+        } else {
+            return res.status(404).send('Reservation not found')
+        }
 
-}).put(async (req, res)=> {
-    let reservations = await Reservations.find({id});
-    reservations = req.body;
+    }).put(async (req, res) => {
+
+        const { date, time } = req.body;
+        let reservation = await Reservations.findByIdAndUpdate({ id }, { date, time });
+        res.status(200).json({ message: 'Reservation updated', reservation });
 
 
-}).delete (async(req,res) => { 
-        let reservation = await Reservations.deleteOne({id})
+    }).delete(async (req, res) => {
+        const { id } = req.params
+        let reservation = await Reservations.findByIdAndDelete({ id });
 
-        return res.status(204).send(`${reservation} deleted`)
+        return res.status(204).send(`${reservation} deleted`);
 
-}) 
+    })
 
 export default router;
